26e6a057cae2686bc158aa6e8dab3d976b583407
Piratux <58703216+Piratux@users.noreply.github.com>
2025-03-11 11:53:37 +0200

    Add TS error checker

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,21 @@ import vue from '@vitejs/plugin-vue'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import eslint from 'vite-plugin-eslint2'
+import checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
     base: '/Pixel-Walker-Copy-Bot/',
-    plugins: [vue(), tsconfigPaths(), nodePolyfills(), eslint()],
+    plugins: [
+      vue(),
+      tsconfigPaths(),
+      nodePolyfills(),
+      eslint(),
+      checker({
+        typescript: true,
+      }),
+    ],
     optimizeDeps: {
       esbuildOptions: {
         target: 'esnext',
