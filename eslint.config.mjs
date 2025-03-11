f47406ea87bb493ef919975e47f255bdf9c912cc
Piratux <58703216+Piratux@users.noreply.github.com>
2025-03-11 13:03:33 +0200

    Add TODO comments

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 
-// Source: https://typescript-eslint.io/getting-started/typed-linting/
-
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 
+// TODO: use vue eslinter https://eslint.vuejs.org/user-guide
+// TODO: setup eslint config prettier, to avoid both fighting for style https://eslint.vuejs.org/user-guide/#example-configuration-with-typescript-eslint-and-prettier
+// TODO: use tseslint.configs.recommendedTypeChecked from https://typescript-eslint.io/getting-started/typed-linting/
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended/*TypeChecked , {
--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,7 @@ export default defineConfig(() => {
       tsconfigPaths(),
       nodePolyfills(),
       eslint(),
+      // TODO: use checker
       // checker({
       //   typescript: true,
       // }),
