2d89dc4110f250bc31e46a5b66f60e11ff086fe7
Piratux <58703216+Piratux@users.noreply.github.com>
2025-03-11 12:05:10 +0200

    Temporarily disable TS checker

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import eslint from 'vite-plugin-eslint2'
-import checker from 'vite-plugin-checker'
+// import checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
@@ -15,9 +15,9 @@ export default defineConfig(() => {
       tsconfigPaths(),
       nodePolyfills(),
       eslint(),
-      checker({
-        typescript: true,
-      }),
+      // checker({
+      //   typescript: true,
+      // }),
     ],
     optimizeDeps: {
       esbuildOptions: {
