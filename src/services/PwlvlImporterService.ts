44e042fca94b0ae30f539b4a6ced041e3bd5864e
Piratux <58703216+Piratux@users.noreply.github.com>
2025-03-15 22:05:58 +0200

    Fix getImportedFromPwlvlData

--- src/services/PwlvlImporterService.ts
+++ src/services/PwlvlImporterService.ts
@@ -3,10 +3,23 @@ import { vec2 } from '@basementuniverse/vec'
 import { placeWorldDataBlocks } from '@/services/WorldService.ts'
 import { getPwGameWorldHelper } from '@/stores/PWClientStore.ts'
 import { sendGlobalChatMessage } from '@/services/ChatMessageService.ts'
-import { pwCheckEditWhenImporting } from '@/services/PWClientService.ts'
+import { pwCheckEditWhenImporting, pwCreateEmptyBlocks } from '@/services/PWClientService.ts'
 
 export function getImportedFromPwlvlData(fileData: ArrayBuffer): DeserialisedStructure {
-  return StructureHelper.read(Buffer.from(fileData))
+  const blocks = pwCreateEmptyBlocks(getPwGameWorldHelper())
+  const importedBlocks = StructureHelper.read(Buffer.from(fileData))
+  const layersInImportedBlocks = importedBlocks.blocks.length
+  const width = Math.min(getPwGameWorldHelper().width, importedBlocks.width)
+  const height = Math.min(getPwGameWorldHelper().height, importedBlocks.height)
+  for (let layer = 0; layer < layersInImportedBlocks; layer++) {
+    for (let x = 0; x < width; x++) {
+      for (let y = 0; y < height; y++) {
+        blocks.blocks[layer][x][y] = importedBlocks.blocks[layer][x][y]
+      }
+    }
+  }
+
+  return blocks
 }
 
 export async function importFromPwlvl(fileData: ArrayBuffer) {
