e05f97a1c07e9b672924bcc63e6c008a8c3196d7
Piratux <58703216+Piratux@users.noreply.github.com>
2025-05-27 19:34:11 +0300

    Fix .move command not shown in .help list

--- src/service/PacketHandlerService.ts
+++ src/service/PacketHandlerService.ts
@@ -285,7 +285,7 @@ function helpCommandReceived(args: string[], playerId: number) {
   if (args.length == 1) {
     sendPrivateChatMessage('Gold coin - select blocks', playerId)
     sendPrivateChatMessage('Blue coin - paste blocks', playerId)
-    sendPrivateChatMessage('Commands: .help .ping .paste .smartpaste .undo .redo .import', playerId)
+    sendPrivateChatMessage('Commands: .help .ping .paste .smartpaste .undo .redo .import .move', playerId)
     sendPrivateChatMessage('See more info about each command via .help [command]', playerId)
     sendPrivateChatMessage('You can also use the bot: piratux.github.io/Pixel-Walker-Copy-Bot/', playerId)
     return
