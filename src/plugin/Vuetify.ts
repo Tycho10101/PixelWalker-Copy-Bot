61dcd3acf819b44365a1320ebea56694e038a381
Piratux <58703216+Piratux@users.noreply.github.com>
2025-06-17 23:56:22 +0300

    Add mdi icons

--- src/plugin/Vuetify.ts
+++ src/plugin/Vuetify.ts
@@ -1,13 +1,16 @@
+import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 const buildVuetify = () =>
   createVuetify({
     ssr: true,
     components,
     directives,
+    icons: { defaultSet: 'mdi', aliases, sets: { mdi } },
   })
 
 export { buildVuetify as createVuetify }
